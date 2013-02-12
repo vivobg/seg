5b49f70f39f5827681a8ed6a3daf4e00f80e9249
Vilian Atmadzhov <vilian.tmadzhov@gmail.com>
2013-02-12 20:06:11 +0000

    Convert Gui class to a JPanel and rename
    Instead of offering a complete JFrame, the class now offers a custom
    JPanel, with example JFrame in the main method. The provided example
    places the custom component into another JPanel, in order to center it
    on to the screen. A scroll pane is also added, in case the map grows
    larger than the containing JFrame.

--- SEG/src/gui/JMapPanel.java
+++ SEG/src/gui/JMapPanel.java
@@ -6,6 +6,8 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -22,97 +24,137 @@ import map.CoordVal;
 import map.Map;
 import map.VerticalArray;
 
-public class Gui extends JFrame implements Observer{
+public class JMapPanel extends JPanel implements Observer{
 	
-	private JPanel jp;
-	private Graphics2D pg;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1361469152515719114L;
+	public static final int blockSize=5;
+	public static final int minWidth = 400;
+	public static final int minHeight = 400;
+	public static final Color COLOR_WALL = Color.BLACK;
+	public static final Color COLOR_EMPTY = Color.GREEN;
+	public static final Color COLOR_UNEXPLORED = Color.GRAY;
+	public static final Color COLOR_UNWALKABLE = Color.ORANGE; //Too close to a wall
 	private Map map;
 	private BuffImg img;
-	//private int x=20;
-	//private int y=20;
-	private int blockSize=5;
 	
-	Gui(Map map){
-		super("Map Gui");
-		img = new BuffImg(1, 1, BufferedImage.TYPE_INT_ARGB);
+	JMapPanel(Map map){
+		img = new BuffImg(1, 1, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2 = img.createGraphics();
+		g2.setColor(COLOR_UNEXPLORED);
+		g2.fillRect(0, 0, img.getWidth(), img.getHeight());
+		g2.dispose();
 		this.map = map;
 		this.map.addObserver(this);
 		updateImage();
-		initWidgets();
+	}
+	
+	/**
+	 * The preferred size of the component is either minWidth,minHeigth
+	 * or the size of the map, whichever is greater
+	 */
+	@Override
+	public Dimension getPreferredSize(){
+		//return new Dimension(img.getWidth()>minWidth?img.getWidth():minWidth,img.getHeight()>minHeight?img.getHeight():minHeight);
+		return new Dimension(img.getWidth(),img.getHeight());
+	}
+	
+	@Override
+	public Dimension getMinimumSize(){ return getPreferredSize();}
+	
+	/**
+	* Draw the generated Buffered Image
+	*
+	**/
+	@Override
+	public void paintComponent (Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img,null, 0, 0);
+		g2.dispose();
+	}
+	/**
+	 * Update and repaint the component on each map update
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		CoordVal cv = (CoordVal) arg;	
+		UpdateImage(cv);
+		this.repaint();
+		this.revalidate();
 	}
 	
 	private void growImage(){
-		//System.out.println(img.getWidth() + " " + img.getHeight());
 		int width = map.getMaxXSize() + map.getMinXSize();
 		int height = map.getMaxYSize() + map.getMinYSize();
 		int centerX = map.getMinXSize();
 		int centerY = map.getMaxYSize();
+		System.out.println(width);
 		//if image is smaller than map
 		if (img.getWidth()<width*blockSize || img.getHeight() < (height+1)*blockSize){
-			BuffImg biggerImg = new BuffImg((width-1)*blockSize+1, (height+1)*blockSize+1, BufferedImage.TYPE_INT_ARGB);
+			BuffImg biggerImg = new BuffImg((width-1)*blockSize+1, (height+1)*blockSize+1, BufferedImage.TYPE_INT_RGB);
 			Graphics2D bg2 = biggerImg.createGraphics();
+			bg2.setColor(COLOR_UNEXPLORED);
+			bg2.fillRect(0, 0, biggerImg.getWidth(), biggerImg.getHeight());
 			bg2.drawImage(img, null, (centerX-img.minX)*blockSize, (centerY-img.MaxY)*blockSize);
 			biggerImg.MaxY = centerY;
 			biggerImg.minX = centerX;
 			img = biggerImg;
 			bg2.dispose();
-		}
-		//System.out.println(img.getWidth() + " " + img.getHeight());
-		
-		
+		}		
 	}
 	
 	private void UpdateImage(CoordVal cv){
-		//System.out.println("SMART update called");
 		if (cv.grown) growImage();
 		int centerX = map.getMinXSize();
 		int centerY = map.getMaxYSize();
 		
 		Graphics2D g2 = img.createGraphics();
-		g2.setColor(Color.blue);
-		float val = cv.value;
-		if (Math.abs(val - 0.5f) < 0.01f) g2.setColor(Color.RED);
-		else g2.setColor(Color.BLUE);
+		g2.setColor(COLOR_EMPTY);
+		if (map.isEmpty(cv.x, cv.y)) g2.setColor(COLOR_EMPTY);
+		else if (map.isUnexplored(cv.x, cv.y))g2.setColor(COLOR_UNEXPLORED);
+		else if (map.isOccupied(cv.x, cv.y))g2.setColor(COLOR_WALL);
+		else g2.setColor(COLOR_UNWALKABLE);
 		g2.fillRect((centerX+cv.x-1)*blockSize, (centerY-cv.y)*blockSize, blockSize, blockSize);
 		g2.dispose();
 	}
 	
 	private void updateImage(){
-		//System.out.println("Dumb update called");
 		//if image is smaller than map
 		growImage();
-		int centerX = map.getMinXSize();
-		int centerY = map.getMaxYSize();
-		//img.minX = centerX*blockSize;
-		//img.MaxY = centerY*blockSize;
+		int minX = map.getMinXSize();
+		//int maxY = map.getMaxYSize();
+		int centerX = minX>0?minX:1;
 		
-		Graphics2D g2 = img.createGraphics();
-		g2.setColor(Color.blue);		
+		int centerY = map.getMaxYSize();
+		
+		Graphics2D g2 = img.createGraphics();		
 		
 		//positive X
 		for (int x=0;x<map.getMaxXSize();x++)
 		{	
 			VerticalArray vert = map.getVertical(x);
-			//System.out.println(vert.toString());
-			for (int i=(vert.getNegSize()-1)*-1;i<vert.getPosSize();i++){
-				float val = vert.getValue(i);
-				if (Math.abs(val - 0.5f) < 0.01f) g2.setColor(Color.RED);
-				else g2.setColor(Color.BLUE);
-				g2.fillRect((centerX+x-1)*blockSize, (centerY-i)*blockSize, blockSize, blockSize);
+			for (int y=(vert.getNegSize()-1)*-1;y<vert.getPosSize();y++){
+				if (map.isEmpty(x, y)) g2.setColor(COLOR_EMPTY);
+				else if (map.isUnexplored(x, y))g2.setColor(COLOR_UNEXPLORED);
+				else if (map.isOccupied(x, y))g2.setColor(COLOR_WALL);
+				else g2.setColor(COLOR_UNWALKABLE);
+				g2.fillRect((centerX+x-1)*blockSize, (centerY-y)*blockSize, blockSize, blockSize);
 			}
 				
 		}
-		//System.out.println("Done positive X");
 		//Negative X
 		for (int x=1;x<map.getMinXSize();x++)
 		{	
 			VerticalArray vert = map.getVertical(-x);
-			//System.out.println(vert.toString());
-			for (int i=(vert.getNegSize()-1)*-1;i<vert.getPosSize();i++){
-				float val = vert.getValue(i);
-				if (Math.abs(val - 0.5f) < 0.01f) g2.setColor(Color.RED);
-				else g2.setColor(Color.BLUE);
-				g2.fillRect((centerX-x)*blockSize, (centerY-i)*blockSize, blockSize, blockSize);
+			for (int y=(vert.getNegSize()-1)*-1;y<vert.getPosSize();y++){
+				if (map.isEmpty(x, y)) g2.setColor(COLOR_EMPTY);
+				else if (map.isUnexplored(x, y))g2.setColor(COLOR_UNEXPLORED);
+				else if (map.isOccupied(x, y))g2.setColor(COLOR_WALL);
+				else g2.setColor(COLOR_UNWALKABLE);
+				g2.fillRect((centerX-x)*blockSize, (centerY-y)*blockSize, blockSize, blockSize);
 			}
 		}
 		g2.dispose();
@@ -120,120 +162,78 @@ public class Gui extends JFrame implements Observer{
 	
 	public Map getMap() {return map;}
 
-	private void initWidgets() {
+	/**
+	 * Example of using the JMapPanel component.
+	 * The component is placed inside a scrollpane to allow
+	 * viewing the whole map, without resizing the parent component/JFrame
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final Map map = new Map();
+		MapChange mc = new MapChange(map);
+		mc.setVisible(true);
+		JFrame frame = new JFrame("Map GUI");
 		
-		jp = new JPanel(){
-			
-			private static final long serialVersionUID = -4960648732508720990L;
-			
-			public Dimension getPreferredSize(){
-				return new Dimension(img.getWidth(),img.getHeight());
-			}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		final JMapPanel jMapPanel = new JMapPanel(map);
+        JPanel p = new JPanel(new GridBagLayout()){
+			private static final long serialVersionUID = 466462159608863092L;
 
-			public void paintComponent (Graphics g){
-				super.paintComponent(g);
-				Graphics2D g2 = (Graphics2D) g;
-				g2.drawImage(img,null, 0, 0);
+			@Override
+        	public Dimension getPreferredSize(){
+				//Dimension size = new Dimension(minWidth, minHeight);
+				Dimension child = jMapPanel.getPreferredSize();
+				int width = child.width>minWidth?child.width:minWidth;
+				int height = child.height>minHeight?child.height:minHeight;
 				
-				/*
-				g2.setColor(Color.blue);
-				//g2.setBackground(Color.RED);
-				//g2.fillRect(x, y, 20, 20);
-				
-				//g2.drawImage(img, op, x, y);
-				
-				int width = map.getMaxXSize() + map.getMinXSize();//-
-				int height = map.getMaxYSize() + map.getMinYSize();//-
-				int centerX = map.getMinXSize();
-				int centerY = map.getMaxYSize();
-				
-				
-				//positive X
-				for (int x=0;x<map.getMaxXSize();x++)
-				{	
-					VerticalArray vert = map.getVertical(x);
-					System.out.println(vert.toString());
-					for (int i=(vert.getNegSize()-1)*-1;i<vert.getPosSize();i++){
-						float val = vert.getValue(i);
-						if (Math.abs(val - 0.5f) < 0.01f) g2.setColor(Color.RED);
-						else g2.setColor(Color.BLUE);
-						g2.fillRect((centerX+x)*blockSize, (centerY-i)*blockSize, blockSize, blockSize);
-					}
-						
-				}
-				System.out.println("Done positive X");
-				//Negative X
-				for (int x=1;x<map.getMinXSize();x++)
-				{	
-					VerticalArray vert = map.getVertical(-x);
-					System.out.println(vert.toString());
-					for (int i=(vert.getNegSize()-1)*-1;i<vert.getPosSize();i++){
-						float val = vert.getValue(i);
-						if (Math.abs(val - 0.5f) < 0.01f) g2.setColor(Color.RED);
-						else g2.setColor(Color.BLUE);
-						g2.fillRect((centerX-x)*blockSize, (centerY-i)*blockSize, blockSize, blockSize);
-					}
-				}*/
-			}
-		};
-        //pg = (Graphics2D) jp.getGraphics();
-        this.setLayout(new BorderLayout());
-        JScrollPane scr = new JScrollPane(jp);
-		this.add(scr, BorderLayout.CENTER);
-		JButton bt = new JButton("Draw");
-		this.add(bt,BorderLayout.NORTH);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(600, 600);
-		
-		
-		
-		bt.addActionListener(new ActionListener() {
+        		return new Dimension(width, height);
+        	}
 			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				jp.repaint();
-				jp.revalidate();
+			public Dimension getMinimumSize(){
+				return getPreferredSize();}
+			};
+		p.setBackground(COLOR_UNEXPLORED);
+        p.add(jMapPanel,new GridBagConstraints());
+        JScrollPane scr = new JScrollPane(p);
+		frame.add(scr, BorderLayout.CENTER);
+		//frame.add(jMapPanel);
+		
+		JButton btn = new JButton("Random values");
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				for (int i=-14;i<15;i+=1){
+					map.setValue(i, (int) (Math.random()*40+10), Map.OCCUPIED);
+					map.setValue(i, (int) -(Math.random()*40+10), Map.EMPTY);
+			}
 				
 			}
 		});
 		
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Map map = new Map();
-		MapChange mc = new MapChange(map);
-		mc.setVisible(true);
-		Gui gui = new Gui(map);
-		gui.setVisible(true);
-		//Component c[] = gui.getComponents();
-		map.setValue(0, 50, 0.3f);
-		map.setValue(0, -50, 0.8f);
-		map.setValue(50, 0, 0.8f);
-		map.setValue(-70, 0, 0.8f);
-		map.setValue(-20, 3, 0.8f);
-		for (int i=-14;i<15;i+=1){
-				map.setValue(i, (int) (Math.random()*40+10), 0.2f);
-				map.setValue(i, (int) -(Math.random()*40+10), 0.2f);
-		}
-		//Gui gui = new Gui(map);
-		//gui.setVisible(true);
+		frame.add(btn, BorderLayout.SOUTH);
 		
+		frame.pack();
+		frame.setVisible(true);
+		
+		/*
+		map.setValue(0, 50, Map.OCCUPIED);
+		map.setValue(0, -50, Map.OCCUPIED);
+		map.setValue(50, 0, Map.OCCUPIED);
+		map.setValue(-50, 0, Map.OCCUPIED);
+		
+		map.setValue(-20, 3, Map.EMPTY);
+		map.setValue(25, 25, Map.UNEXPLORED);
+		map.setValue(-43, 35, Map.EMPTY);
+		*/
+		
+		System.out.println(map.getValue(0, 50));
+		System.out.println(map.getValue(0, -50));
 
 	}
 
-	@Override
-	public void update(Observable o, Object arg) {
-		CoordVal cv = (CoordVal) arg;
-		jp.setDoubleBuffered(true);
-		
-		UpdateImage(cv);
-		jp.repaint();
-		jp.revalidate();
-		
-	}
+	
 
 }
