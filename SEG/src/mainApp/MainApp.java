d8a2eeefb4db81e89322a05dece214c119d6b545
abdi <aossoble@gmail.com>
2013-02-20 23:36:08 +0000

    Change to use Control Module

--- SEG/src/mainApp/MainApp.java
+++ SEG/src/mainApp/MainApp.java
@@ -6,15 +6,9 @@ import javax.swing.JFrame;
 public class MainApp
 {
 	
-	static double forwardSpeed = 0.5;
-	//This is the main method
+	
 	public static void main(String[] args) {
-		MainFrame frame = new MainFrame();
-		frame.setTitle("Main Control");
-		frame.setSize(500,500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-       
+		Control control = new Control(args);
 	  }
 	}
 	
