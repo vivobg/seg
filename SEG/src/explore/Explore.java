407a8ddb711056aa1dfaa23980407ffc2f9a60ea
Cassio Cordeiro <k1189735@kcl.ac.uk>
2013-02-22 19:26:50 +0000

    explore method

--- SEG/src/explore/Explore.java
+++ SEG/src/explore/Explore.java
@@ -1,6 +1,15 @@
 package explore;
 
+import robot.Robot;
+import map.Map;
+
 public class Explore {
 
+	/**
+	 * Method responsible for exploring the environment
+	 */
+	public void explore (Map m , Robot b ){
+		
+	}
 	
 }
