8227798ea53473ee2872c9b2d20cb7da280cb11c
Vilian Atmadzhov <vilian.atmadzhov@gmail.com>
2013-03-12 17:27:18 +0000

    Extract Bresenham algorithm in a separate method. Reduce code repetition.

--- SEG/src/sense/Bresenham.java
+++ SEG/src/sense/Bresenham.java
@@ -26,25 +26,8 @@ public class Bresenham {
 	public static void line(Map map, int x0, int y0, int x1, int y1,
 			boolean WALL) {
 
-		int dx = Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-		int dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-		int err = (dx > dy ? dx : -dy) / 2, e2;
-
-		ArrayList<Point> points = new ArrayList<Point>();
-		for (;;) {
-			points.add(new Point(x0, y0));
-			if (x0 == x1 && y0 == y1)
-				break;
-			e2 = err;
-			if (e2 > -dx) {
-				err -= dy;
-				x0 += sx;
-			}
-			if (e2 < dy) {
-				err += dx;
-				y0 += sy;
-			}
-		}
+		ArrayList<Point> points = bresenhamLine(x0, y0, x1, y1);
+		
 		if (WALL) {
 			for (int i = 0; i < points.size() - Map.UNWALKABLE_CELLS; i++) {
 				Point p = points.get(i);
@@ -67,6 +50,29 @@ public class Bresenham {
 				map.setValue(p.x, p.y, Map.EMPTY);
 			}
 		}
+	}
+	
+	public static ArrayList<Point> bresenhamLine(int x0, int y0, int x1, int y1){
+		int dx = Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+		int dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+		int err = (dx > dy ? dx : -dy) / 2, e2;
+
+		ArrayList<Point> points = new ArrayList<Point>();
+		for (;;) {
+			points.add(new Point(x0, y0));
+			if (x0 == x1 && y0 == y1)
+				break;
+			e2 = err;
+			if (e2 > -dx) {
+				err -= dy;
+				x0 += sx;
+			}
+			if (e2 < dy) {
+				err += dx;
+				y0 += sy;
+			}
+		}
+		return points;
 	}
 
 	private static void circle(Map map, int x0, int y0, int radius) {
