21f6d34c3cd323e295297f225c8429c5a862e4a6
Vilian Atmadzhov <vilian.atmadzhov@gmail.com>
2013-03-20 17:46:13 +0000

    Remove commented code from Explore.explore method

--- SEG/src/explore/Explore.java
+++ SEG/src/explore/Explore.java
@@ -1,7 +1,6 @@
 package explore;
 
 import java.awt.Point;
-import java.util.ArrayList;
 import java.util.List;
 
 import map.Map;
@@ -16,10 +15,8 @@ public class Explore {
 	 * @param start the starting point of the exploration
 	 */
 	public static void explore(Map map,Robot robot, Point start) {
-		//System.out.println("Exploration started");
 		List<Point> path = null;
 		do {
-			//scan(robot);
 			path = Search.dSearch(map, start);
 			robot.currentPath = path;
 			if (path != null) {
@@ -28,37 +25,15 @@ public class Explore {
 				robot.isFollowing = true;
 				for (int i = 1; i<path.size();i++){
 					
-				
 					Point p = path.get(i);
 					Point end = path.get(path.size()-1);
 
-					// if (!map.isUnexplored(end.x, end.y)
-					// || !Search.isAvailableCell(end, map)) {
-					// break;
-					// }
-					robot.move(p, end);// Decide if worth moving, before moving
-					// try {
-					//
-					// Thread.sleep(30);
-					// } catch (InterruptedException e) {
-					// // TODO Auto-generated catch block
-					// e.printStackTrace();
-					// }
+					robot.move(p, end);
 				}
-				start = Map.convertPlayerToInternal(robot.x, robot.y);			
-
-				// try {
-				//
-				// Thread.sleep(30);
-				// } catch (InterruptedException e) {
-				// // TODO Auto-generated catch block
-				// e.printStackTrace();
-				// }
+				start = Map.convertPlayerToInternal(robot.x, robot.y);
 			}
 		} while (path != null);
 		robot.isFollowing = false;
-		//System.out.println("Exploration finished");
-
 	}
 
 	
